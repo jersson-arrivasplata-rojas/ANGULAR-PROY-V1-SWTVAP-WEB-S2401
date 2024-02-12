ccbaf27dc7cfd7fd476a3c7eada3081a23229104
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-02-12 18:42:22 -0500

    Add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BaseComponent } from './feature/components/base/base.component';
 import { FeatureModule } from './feature/feature.module';
 
+registerLocaleData(localeEs);
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +21,9 @@ import { FeatureModule } from './feature/feature.module';
     FeatureModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
--- src/app/shared/directives/date-format.directive.ts
+++ src/app/shared/directives/date-format.directive.ts
@@ -15,7 +15,12 @@ export class DateFormatDirective {
   }
 
   @HostListener('input', ['$event.target.value'])
-  onInput(value: string) {
+  onInput(value: string) {   // Verificar si el valor contiene solo números y guiones
+    if (!/^[\d-]*$/.test(value)) {
+      this.ngControl.control?.setValue(this.previousValue, { emitEvent: false });
+      return;
+    }
+
     if (value.length > 10) {
       this.ngControl.control?.setValue(value.slice(0, 10), { emitEvent: false });
     } else if ((value.length === 4 || value.length === 7) && /\d$/.test(value) && value.length > this.previousValue.length) {
